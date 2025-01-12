87df6ccc84115bb597844528ae6084945b17c33c
Ko Min-jae <146058723+ourOzone@users.noreply.github.com>
2025-01-12 23:41:08 +0900

    으아아악 연등 끗
    자고싶다......

--- write_page.js
+++ write_page.js
@@ -1,9 +1,42 @@
 const writer = document.getElementById("writer");
 const add_button = document.getElementById("adder");
-add_button.onclick = add_new_block;
+add_button.onclick = add_new_line;
 
-function add_new_block() {
+id_cnt = 0
+function add_new_line() {
+    const line = document.createElement("div");
+    line.className = "line"
+    
+    const menu = document.createElement("div");
+    menu.className = "menu"
+    menu.textContent = "여기에 메뉴바가 들어가요(글 수정모드, 너비/위치 조작모드, 블럭 추가, 라인 삭제)";
+
+    id_cnt += 1;
+    const line_id = "" + id_cnt;
+    line.id = "" + line_id
+
+    menu.onclick = () => add_block(line_id, "text");
+    line.append(menu);
+
+
+    writer.append(line);
+}
+
+function add_block(id, type) {
+    const mother_line = document.getElementById(id);
+    const child_cnt = mother_line.childElementCount;
+    const block_id = id + "t" + child_cnt;
+
     const block = document.createElement("div");
-    block.textContent = "temp";
-    writer.append(block);
+    block.className = "block";
+    block.id = block_id;
+    
+    if (type == "text") {
+        const text = document.createElement("input");
+        block.append(text);
+    }
+
+    const line = document.getElementById(id);
+    line.append(block);
+
 }
