169cd59069b995ad738b8e7e04a31ebab5b33810
Ko Min-jae <146058723+ourOzone@users.noreply.github.com>
2025-01-13 23:00:41 +0900

    오늘은 글쓰기 칸을 대충 완성했어요.
    지금 퀄을 좀 더 올리고 DB를 만들어야할지
    아니면 일단 DB를 만들고 퀄을 올릴지 고민중이에요
    
    지금 상당히 대충 만들어서 기능들이 부족한 상황인데
    기능들을 어느정도 완성하고 어떻게 할지 좀 더 고민해야 할 거 같아요
    
    으아아악 쉽지않다

--- write_page.js
+++ write_page.js
@@ -11,13 +11,20 @@ function add_new_line() {
     menu.className = "menu"
     menu.textContent = "여기에 메뉴바가 들어가요(글 수정모드, 너비/위치 조작모드, 블럭 추가, 라인 삭제)";
 
+    const block_container = document.createElement("div");
+    block_container.className = "block_container";
+
+
     id_cnt += 1;
     const line_id = "" + id_cnt;
-    line.id = "" + line_id
+    line.id = "" + line_id;
+    const block_container_id = "b" + line_id;
 
-    menu.onclick = () => add_block(line_id, "text");
+    block_container.id = block_container_id;
+    menu.onclick = () => add_block(block_container_id, "text");
     line.append(menu);
 
+    line.append(block_container);
 
     writer.append(line);
 }
@@ -32,11 +39,34 @@ function add_block(id, type) {
     block.id = block_id;
     
     if (type == "text") {
-        const text = document.createElement("input");
+        const text = document.createElement("textarea");
         block.append(text);
     }
 
     const line = document.getElementById(id);
     line.append(block);
 
+}
+
+function submit_write() {
+    const lines = document.getElementsByClassName("line");
+    const linesArray = Array.from(lines);
+
+    blocks_value = []
+
+    linesArray.forEach((e) => {
+        temp = []
+        const block_container = e.children[1];
+        const blocks = Array.from(block_container.children);
+
+        blocks.forEach((e) => {
+            const value = e.children[0].value;
+            temp.push(value)
+        })
+        blocks_value.push(temp);
+        
+    });
+
+    console.log(blocks_value);
+    return blocks_value;
 }
